a64643930436202070fc1ed414d670441806dad4
sureshsangineedi <suresh.sangineedi@outlook.com>
2026-02-04 12:20:43 +0530

    adding rod folder

--- day2/app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("🚀 Day-2 Jenkins CI/CD Working!");
-});
-
-app.listen(3000, () => console.log("Server started on 3000"));
